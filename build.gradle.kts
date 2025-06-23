201bb3a484a63b5632b72f3a43a4ca466a8e135d
santannaf <thales.farias.santanna@gmail.com>
2025-06-23 09:44:06 -0300

    upgrade app

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,6 +54,8 @@ dependencies {
 
     // Unit Tests
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 kotlin {
